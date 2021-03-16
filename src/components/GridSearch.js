9d07a932eddad2a4055299ada755fa59a3a4b7bb
YassineSajjade <yassinesajjade@gmail.com>
2021-03-16 19:22:23 +0100

    some update

--- src/components/GridSearch.js
+++ src/components/GridSearch.js
@@ -20,8 +20,8 @@ function GridSearch() {
     return (
         <>
             <div className="container" style={{ marginTop: "10px" }}>
-                <div className="row">
 
+                <div className="row">
                     <div className="col-lg-3 col-md-6 col-left" onClick={showAllCat}>
                         <span>All Categories</span>
                         <i className="fas fa-angle-down"></i>
@@ -33,11 +33,21 @@ function GridSearch() {
                             <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                         </form>
                     </div>
-
+                </div>
+                <div className="row row-col-list">
+                    <div className="col-list" style={{ display: display }}>
+                        <ul>
+                            <li>Fruits</li>
+                            <li>Vegitables</li>
+                            <li>Flour</li>
+                            <li>Cookies</li>
+                            <li>Meat</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
 
-            <div className="container">
+            {/* <div className="container">
                 <div className="row row-col-list">
                     <div className=" col-list" style={{ display: display }}>
                         <ul>
@@ -49,12 +59,12 @@ function GridSearch() {
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div> */}
                 
                     
 
 
-            <div id="carouselExampleCaptions" className="row carousel " style={{ marginTop: "30px" }}>
+            <div id="carouselExampleCaptions" className="carousel " style={{ marginTop: "30px" }}>
 
                 <ol className="carousel-indicators">
                     <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
@@ -62,18 +72,18 @@ function GridSearch() {
                     <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
                 </ol>
                 <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-1_2000x.jpg?v=1581341041" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block slider1">
+                    <div className="carousel-item citem1 mobile-item1 active">
+                        {/* <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-1_2000x.jpg?v=1581341041" className="d-block w-100" alt="..." /> */}
+                        <div className="carousel-caption  d-md-block slider1">
                             <h5 style={{ color: "#000000", fontSize: "18px" }}>Natural Health care Ingredients</h5>
                             <h2 style={{ color: "#d08126", fontWeight: "bold", fontFamily: "Yeseva One, cursive" }}>Grocery Shopping</h2>
                             <h4 style={{ color: "#000000", fontSize: "26px" }}>Save upto 30% off</h4>
                             <Link className="slider1-link btn" to="#" style={{ background: "#d8943a", color: "#ffffff", borderRadius: "30px" }}>Shop now</Link>
                         </div>
                     </div>
-                    <div className="carousel-item">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-2_2000x.jpg?v=1581341045" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block slider2">
+                    <div className="carousel-item citem2 mobile-item2">
+                        {/* <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-2_2000x.jpg?v=1581341045" className="d-block w-100" alt="..." /> */}
+                        <div className="carousel-caption  d-md-block slider2">
                             <h5 style={{ color: "#000000", fontSize: "18px", fontWeight: "600", fontFamily: "Poppins", textTransform: "uppercase" }}>Groca</h5>
                             <h2 style={{ color: "red", fontWeight: "bold", fontFamily: "Yeseva One, cursive", fontSize: "30px" }}>Vegetable 100% organic</h2>
                             <h4 style={{ color: "#000000", fontSize: "18px", fontFamily: "Poppins" }}>Natural Health Care Ingredients</h4>
@@ -81,10 +91,10 @@ function GridSearch() {
                             <Link className="slider2-link btn" to="#" style={{ background: "#ffffff", color: "red", borderRadius: "30px" }}>Shop now</Link>
                         </div>
                     </div>
-                    <div className="carousel-item">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-3_2000x.jpg?v=1581341055" className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block slider3">
-                            <div className="icon" style={{ marginBottom: "10px" }}><img src="//cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-icon_100X.png?v=1581426760" /></div>
+                    <div className="carousel-item citem3 mobile-item3">
+                        {/* <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-3_2000x.jpg?v=1581341055" className="d-block w-100" alt="..." /> */}
+                        <div className="carousel-caption d-md-block slider3">
+                            <div className="icon" style={{ marginBottom: "10px" }}><img src="//cdn.shopify.com/s/files/1/0108/7370/0415/files/slider-icon_100X.png?v=1581426760" style={{ height: "100%"}}/></div>
                             <h5 style={{ color: "#000000", fontSize: "18px" }}>100% Healthy & Affordable</h5>
                             <h2 style={{ color: "#4a8601", fontWeight: "bold", fontFamily: "Yeseva One, cursive" }}>Organic Vegitables</h2>
                             <h4 style={{ color: "#000000", fontSize: "26px" }}>Small Changes Big Differnce</h4>
