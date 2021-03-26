45b8a664f84940b8d29af7cf8033b83aab2fd6e7
YassineSajjade <yassinesajjade@gmail.com>
2021-03-26 19:32:00 +0100

    add ourProducts

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
         <>
           <Navbar/>
           <GridSearche/>  
-          {/* <OurProducts/> */}
+          <OurProducts/>
         </>
     )
 }
--- src/components/OurProducts.js
+++ src/components/OurProducts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./OurProducts.css";
 
 function OurProducts() {
@@ -8,6 +9,46 @@ function OurProducts() {
                 <h2>Our Products</h2>
                 <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry</div>
                 <span></span>
+            </div>
+
+            <div className="brand-names row" style={{margin: "auto"}}>
+                <div className="brand-logo col">
+                    <Link to="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
+                        <span>Milk</span>
+                    </Link>
+                </div>
+                <div className="brand-logo col">
+                    <Link to="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
+                        <span>Milk</span>
+                    </Link>
+                </div>
+                <div className="brand-logo col">
+                    <Link to="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
+                        <span>Milk</span>
+                    </Link>
+                </div>
+                <div className="brand-logo col">
+                    <Link to="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
+                        <span>Milk</span>
+                    </Link>
+                </div>
+                <div className="brand-logo col">
+                    <Link to="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
+                        <span>Milk</span>
+                    </Link>
+                </div>
+                <div className="brand-logo col">
+                    <Link to="/">
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
+                        <span>Milk</span>
+                    </Link>
+                </div>
+
             </div>
         </>
     )
