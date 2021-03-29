631624ae398090b1d9e2e5738071fce826cac346
YassineSajjade <yassinesajjade@gmail.com>
2021-03-29 18:06:25 +0100

    add background for ourproduct section

--- src/components/OurProducts.js
+++ src/components/OurProducts.js
@@ -12,40 +12,40 @@ function OurProducts() {
             </div>
 
             <div className="brand-names row" style={{margin: "auto"}}>
-                <div className="brand-logo col">
+                <div className="brand-logo col" style={{background:"#e1ffd9"}}>
                     <Link to="/">
                         <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
                         <span>Milk</span>
                     </Link>
                 </div>
-                <div className="brand-logo col">
+                <div className="brand-logo col" style={{background:"#ffdedc"}}>
                     <Link to="/">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
-                        <span>Milk</span>
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-5_medium.png?v=1580284176"/>
+                        <span>Fruits</span>
                     </Link>
                 </div>
-                <div className="brand-logo col">
+                <div className="brand-logo col" style={{background:"#dff1ff"}}>
                     <Link to="/">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
-                        <span>Milk</span>
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-4_medium.png?v=1580284192"/>
+                        <span>Flour</span>
                     </Link>
                 </div>
-                <div className="brand-logo col">
+                <div className="brand-logo col" style={{background:"#fbf9c9"}}>
                     <Link to="/">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
-                        <span>Milk</span>
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-3_medium.png?v=1580284218"/>
+                        <span>Meat</span>
                     </Link>
                 </div>
-                <div className="brand-logo col">
+                <div className="brand-logo col" style={{background:"#ffd8ed"}}>
                     <Link to="/">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
-                        <span>Milk</span>
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-2_medium.png?v=1580284239"/>
+                        <span>Vegies</span>
                     </Link>
                 </div>
-                <div className="brand-logo col">
+                <div className="brand-logo col" style={{background:"#e1e9fc"}}>
                     <Link to="/">
-                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-6_medium.png?v=1580284143"/>
-                        <span>Milk</span>
+                        <img src="https://cdn.shopify.com/s/files/1/0108/7370/0415/files/icon-1_medium.png?v=1580284251"/>
+                        <span>Eggs</span>
                     </Link>
                 </div>
 
