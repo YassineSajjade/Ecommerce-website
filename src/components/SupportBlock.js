ac7151c599b40c7017613ed6add2323929dc5a0d
YassineSajjade <yassinesajjade@gmail.com>
2021-04-23 14:21:04 +0000

    update supportSection

--- src/components/SupportBlock.js
+++ src/components/SupportBlock.js
@@ -7,24 +7,22 @@ function SupportBlock() {
         <>
             <div className="grid-uniform">
                 <div className="container">
-                    {/* start ul */}
-                    <ul className="support-block" style={{ background: "#28a745" }}>
 
-                        <li className="grid-item  animate__animated  animate__fadeInUp">
-                            <div className="support-section ">
-                                <div className="support-icon" style={{ background: "#ffffff" }}>
-                                    <Link className="icon" to="#" style={{ color: "#28a745" }}>
-                                        <i className="fas fa-truck"></i>
-                                    </Link>
-                                </div>
-                                <div className="support-text">
-                                    <h5 style={{ color: "#ffffff" }}>Free Shipping</h5>
-                                    <p className="desc" style={{ color: "#ffffff" }}>WorldWide</p>
-                                </div>
+                    <div className="row " style={{ background: "#28a745" }}>
+
+                        <div className="col-6 col-lg-3 support-section animate__animated  animate__fadeInUp">
+                            <div className="support-icon" style={{ background: "#ffffff" }}>
+                                <Link className="icon" to="#" style={{ color: "#28a745" }}>
+                                    <i className="fas fa-truck"></i>
+                                </Link>
                             </div>
-                        </li>
+                            <div className="support-text">
+                                <h5 style={{ color: "#ffffff" }}>Free Shipping</h5>
+                                <p className="desc" style={{ color: "#ffffff" }}>WorldWide</p>
+                            </div>
+                        </div>
 
-                        <li className="grid-item  animate__animated  animate__fadeInUp">
+                        <div className="col-6 col-lg-3 support-section ">
                             <div className="support-section">
                                 <div className="support-icon" style={{ background: "#ffffff" }}>
                                     <Link className="icon" to="#" style={{ color: "#28a745" }}>
@@ -36,9 +34,9 @@ function SupportBlock() {
                                     <p className="desc" style={{ color: "#ffffff" }}>+(000)123-456</p>
                                 </div>
                             </div>
-                        </li>
+                        </div>
 
-                        <li className="grid-item  animate__animated  animate__fadeInUp">
+                        <div className="col-6 col-lg-3 support-section ">
                             <div className="support-section">
                                 <div className="support-icon" style={{ background: "#ffffff" }}>
                                     <Link className="icon" to="#" style={{ color: "#28a745" }}>
@@ -50,9 +48,9 @@ function SupportBlock() {
                                     <p className="desc" style={{ color: "#ffffff" }}>Customer Support</p>
                                 </div>
                             </div>
-                        </li>
+                        </div>
 
-                        <li className="grid-item  animate__animated  animate__fadeInUp">
+                        <div className="col-6 col-lg-3 support-section ">
                             <div className="support-section">
                                 <div className="support-icon" style={{ background: "#ffffff" }}>
                                     <Link className="icon" to="#" style={{ color: "#28a745" }}>
@@ -64,10 +62,10 @@ function SupportBlock() {
                                     <p className="desc" style={{ color: "#ffffff" }}>and Exchange</p>
                                 </div>
                             </div>
-                        </li>
+                        </div>
 
-                    </ul>
-                    {/* end ul */}
+                    </div>
+
                 </div>
             </div>
             <div className="invisible-medium"></div>
